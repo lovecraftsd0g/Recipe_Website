12aa24e4d7129a33193929e986d81884ba07942a
the_str4ng3m4n <144786427+lovecraftsd0g@users.noreply.github.com>
2024-08-04 17:19:41 +0700

    Add files via upload

--- something.js
+++ something.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
-    $("#gun").click(function(){
-        $("#you").hide();
-    })
-})
+function searchfood(){
+    let input = document.getElementById('searchbar').value.toLowerCase();
+    let cards = document.getElementsByClassName('recipe-card');
+    let names = document.getElementsByClassName('food-name');
+    
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i].textContent.toLowerCase();
+        if (name.includes(input)) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
